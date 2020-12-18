d6d865a56b321baff88fdf999b989ea91ec468fc
David DiPaola <365233-DavidDiPaola@users.noreply.gitlab.com>
2020-12-18 09:40:25 -0800

    main: initialize main clock source.

--- main.c
+++ main.c
@@ -8,6 +8,8 @@ Licensed under CC0
 
 int
 main(void) {
+	rcc_clock_setup_in_hse_8mhz_out_72mhz();
+
 	rcc_periph_clock_enable(RCC_GPIOC);
 	gpio_set_mode(GPIOC, GPIO_MODE_OUTPUT_2_MHZ, GPIO_CNF_OUTPUT_PUSHPULL, GPIO13);
 	gpio_set(GPIOC, GPIO13);
